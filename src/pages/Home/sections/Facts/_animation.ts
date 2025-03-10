7102b2aee27bbcb1af8f9361b7490961b53e197a
ajcent <johnloyd.centeno@gmail.com>
2025-03-10 20:46:19 +0800

    Change: animation approach

--- src/pages/Home/sections/Facts/_animation.ts
+++ src/pages/Home/sections/Facts/_animation.ts
@@ -3,7 +3,7 @@ import { AnimationParams } from "@/hooks/useAnimation";
 const animation: AnimationParams[] = [
   ["h6", { opacity: [0, 1], y: [25, 0] }, { duration: 0.3 }],
   ["p", { opacity: [0, 1], y: [25, 0] }, { duration: 0.3 }],
-  ["figure", { opacity: [0, 1], y: [25, 0] }],
+  ["figure", { opacity: [0, 1], y: [25, 0] }, { duration: 0.3 }],
 ];
 
 export default animation;
