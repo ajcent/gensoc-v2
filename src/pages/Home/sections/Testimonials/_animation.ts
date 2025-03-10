57651e81387d64714b9da403fd4bffc80b7a6839
ajcent <johnloyd.centeno@gmail.com>
2025-03-10 20:44:22 +0800

    Change: animation approach

--- src/pages/Home/sections/Testimonials/_animation.ts
+++ src/pages/Home/sections/Testimonials/_animation.ts
@@ -5,17 +5,17 @@ const animation: AnimationParams[] = [
   [
     "#testimonial-content h2",
     { opacity: [0, 1], x: [-50, 0] },
-    { duration: 0.2 },
+    { duration: 0.3 },
   ],
   [
     "#testimonial-content p",
     { opacity: [0, 1], x: [-50, 0] },
-    { duration: 0.2 },
+    { duration: 0.3 },
   ],
   [
     "article",
     { opacity: [0, 1], y: [50, 0] },
-    { duration: 0.2, delay: stagger(0.2) },
+    { duration: 0.3, delay: stagger(0.3) },
   ],
 ];
 
